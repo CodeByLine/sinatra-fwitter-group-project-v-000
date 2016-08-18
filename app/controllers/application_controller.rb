d3334ec313e9a3ed51646ec56d450c9953fe8421
CodeByLine <yleventhal@ymail.com>
2016-08-18 18:01:41 -0400

    Half of the code is not mine-something weird is going on. Trying to debug with known good code, but not successful getting help now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    if logged_in?
+      erb :'/tweets'
+    else
+
+      erb :'index'
+    end
+
   end
 
   helpers do
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to :'/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == '' || params[:email] =='' || params[:password]==''
+      redirect to '/signup'
+    else
+      @user = User.new(:username=> params[:username], email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    end
+
+  end
+
+  get '/login' do
+    if session[:user_id]
+      redirect to '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:id])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to 'users/signup'
+    end
+  end
+
+
+  get '/logout' do
+    session.destroy
+    redirect to :'/login'
+  end
+
+end
